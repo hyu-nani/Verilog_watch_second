65263460654f403d149debf38ef59912b11dad17
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-05-28 20:47:04 +0900

    Revert "수정"
    This reverts commit 5c90499f0c60a756e078367abe79fbd30c38518a.

--- Verilog_watch_second.v
+++ Verilog_watch_second.v
@@ -28,22 +28,16 @@ module Verilog_watch_second(
 		.second		(second));
 	
 	bin2BCD					CON0(
-		.clk			(clk),
-		.rst			(rst),
 		.bin			(hour),
 		.tens			(tenH),
 		.ones			(oneH));
 		
 	bin2BCD					CON1(
-		.clk			(clk),
-		.rst			(rst),
 		.bin			(minute),
 		.tens			(tenM),
 		.ones			(oneM));
 	
 	bin2BCD					CON2(
-		.clk			(clk),
-		.rst			(rst),
 		.bin			(second),
 		.tens			(tenS),
 		.ones			(oneS));
@@ -56,12 +50,12 @@ module Verilog_watch_second(
    lcd_display_string 	STR(
       .clk    		(clk),
       .rst    		(rst),
-		.tenH			(tenH),
-		.oneH			(oneH),
-		.tenM			(tenM),
-		.oneM			(oneM),
-		.tenS			(tenS),
-		.oneS			(oneS),
+		.tenH			(4'd1),
+		.oneH			(4'd1),
+		.tenM			(4'd3),
+		.oneM			(4'd5),
+		.tenS			(4'd5),
+		.oneS			(4'd2),
       .index  		(index_char),
       .out    		(data_char));
       
--- bin2BCD.v
+++ bin2BCD.v
@@ -1,11 +1,8 @@
 module bin2BCD(
-	clk,
-	rst,
 	bin,
 	tens,
 	ones);
 	
-	input			clk,rst;
 	input	[7:0] bin;
 	output[3:0]	tens,ones;
 	
@@ -14,28 +11,22 @@ module bin2BCD(
 	
 	integer	i;
 	
-	always @(posedge clk or negedge rst) begin
-		if(!rst) begin
-			tens	<=	4'd0;
-			ones	<=	4'd0;
+	always @(bin) begin
+		tens		=	4'd0;
+		ones		=	4'd0;
+		for (i=7;i>=0;i=i-1) begin
+			if(tens >= 5)
+				tens 		= tens + 4'b0011;
+			if(ones >= 5)
+				ones 		= ones + 4'b0011;
+				
+			tens			= tens	<< 1;
+			tens[0] 		= ones[3];
+			ones 			= ones << 1;
+			ones[0] 		= bin[i];
 		end
-		else begin
-			tens		=	4'd0;
-			ones		=	4'd0;
-			for (i=7;i>=0;i=i-1) begin
-				if(tens >= 5)
-					tens 		= tens + 4'b0011;
-				if(ones >= 5)
-					ones 		= ones + 4'b0011;
-					
-				tens			= tens	<< 1;
-				tens[0] 		= ones[3];
-				ones 			= ones << 1;
-				ones[0] 		= bin[i];
-			end
-			tens		<=	tens;
-			ones		<=	ones;
-		end
+		tens		<=	tens;
+		ones		<=	ones;
 	end
 	
 endmodule
